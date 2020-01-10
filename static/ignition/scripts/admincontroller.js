3c6a046287baca8f92fda90bace1b157e1785d34
zengelan <andreas.zengel@gmail.com>
2020-01-10 20:21:07 +0100

    updates to admin page

--- static/ignition/scripts/admincontroller.js
+++ static/ignition/scripts/admincontroller.js
@@ -1,12 +1,26 @@
 var redisApp = angular.module('admin', ['ui.bootstrap','ngCookies']);
-var apiUrl = "https://nfk3jyz99k.execute-api.us-east-1.amazonaws.com/get-sesummit2020-creds-prod/v2?mfeemail=none%40mcafee.com&acckey=14653d20-3889-4187-9d82-ada2eebfd524&adminkey=2c2d4b56-80c6-4d68-8cc9-adf909da1edf"
-var loadingdiv = $('#loading');
-var resulttable = $('#resulttable');
+var apiUrl = "https://nfk3jyz99k.execute-api.us-east-1.amazonaws.com/get-sesummit2020-creds-prod/v2?mfeemail=none%40mcafee.com&acckey=14653d20-3889-4187-9d82-ada2eebfd524"
+var loadingdiv;
+var resulttable;
+
+$('#saveadminkey').on('click', function (e) {
+    Cookies.set('adminkey', $('#adminkeyin').val(), { expires: 60 });
+    if (Cookies.get('adminkey')){
+        $('#adminkeydiv').hide();
+    } else {
+        $('#adminkeydiv').show();
+    }
+})
 
 function bodyOnLoad(){
     loadingdiv = $('#loading');
     resulttable = $('#resulttable');
     resulttable.hide();
+    if (Cookies.get('adminkey')){
+        $('#adminkeydiv').hide();
+    } else {
+        $('#adminkeydiv').show();
+    }
 }
 /**
  * Constructor
@@ -14,10 +28,34 @@ function bodyOnLoad(){
 function RedisController() {}
 
 RedisController.prototype.onUserStatus = function() {
-    this.http_.get(apiUrl + "&admin=get_users")
-            .success(angular.bind(this, function(data) {
-                this.scope_.redisResponse = "Updated.";
-            }));
+    this.http_.get(apiUrl + "&admin=get_user_table")
+            .success(function(data) {
+                console.log(data);
+                this.user_table = data.user_table;
+                this.update_time = data.update_time;
+                loadingdiv.hide();
+                resulttable.show();
+            })
+            .error(function(resp) {
+                console.log(resp);
+                $('#adminkeydiv').show();
+                loadingdiv[0].innerText="ERROR!";
+            });
+};
+
+RedisController.prototype.onGetJenkins = function() {
+    this.http_.get(apiUrl + "&admin=get_jenkins")
+        .success(function(data) {
+            console.log(data);
+            this.jenkins = data.jenkins;
+            loadingdiv.hide();
+            resulttable.show();
+        })
+        .error(function(resp) {
+            console.log(resp);
+            $('#adminkeydiv').show();
+            loadingdiv[0].innerText="ERROR!";
+        });
 };
 
 redisApp.controller('AdminCtrl', function ($scope, $http, $location) {
@@ -25,14 +63,38 @@ redisApp.controller('AdminCtrl', function ($scope, $http, $location) {
         $scope.controller.scope_ = $scope;
         $scope.controller.location_ = $location;
         $scope.controller.http_ = $http;
-
+        
+        loadingdiv = $('#loading');
+        resulttable = $('#resulttable');
         loadingdiv.show();
+        apiUrl = apiUrl + "&adminkey=" + Cookies.get('adminkey');
+
+
+        $scope.controller.http_.get(apiUrl + "&admin=get_jenkins")
+            .success(function(data) {
+                console.log(data);
+                $scope.jenkins = data.jenkins;
+                console.log($scope.jenkins);
+            })
+            .error(function(resp) {
+                console.log(resp);
+                $('#adminkeydiv').show();
+                loadingdiv[0].innerText="ERROR!";
+            });
+
+
         $scope.controller.http_.get(apiUrl + "&admin=get_user_table")
             .success(function(data) {
                 console.log(data);
                 $scope.user_table = data.user_table;
+                $scope.update_time = data.update_time;
                 loadingdiv.hide();
                 resulttable.show();
+            })
+            .error(function(resp) {
+                console.log(resp);
+                $('#adminkeydiv').show();
+                loadingdiv[0].innerText="ERROR!";
             });
 
 });
