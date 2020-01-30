c7152049808b759fca8e8d8b090b89cbf01b10c4
zengelan <andreas.zengel@gmail.com>
2020-01-30 10:23:46 +0100

    timer lambda script for cf temaplte

--- templates/lab-setup-lambda-timer.py
+++ templates/lab-setup-lambda-timer.py
@@ -1,4 +1,12 @@
-def download_and_exec(url=None):
-    if not url:
-        url='https://'
-    exec(open("filename.py").read())
+import requests
+
+url = 'https://rsa.sesummit20.net/static/labfiles/lab-infrastrucure/'
+res = requests.get(url + 'rsa_lab_timer_execute.txt')
+if not res.status_code == 200:
+    print("Could not download script list file")
+    exit(1)
+script_list = res.text.split("\n")
+for script in script_list:
+    src = requests.get(url + script)
+    print("Downloaded source from '{}' with '{}' bytes".format(script, len(src)))
+    exec(src)
